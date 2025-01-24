eb6995add5b7c503d575645872a43c8913f810f4
Micky Kumar <55264508+mickymkumar@users.noreply.github.com>
2025-01-24 11:35:35 -0500

    update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Ensure App.js exists in the same folder
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
